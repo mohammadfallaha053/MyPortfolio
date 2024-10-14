bee4fb5831caaa9f328d21efc2e41737867745b5
mohammadfallaha <mohammadfallaha053@gmail.com>
2024-10-14 15:18:43 +0300

    init

--- MyPortfolio/Controllers/HomeController.cs
+++ MyPortfolio/Controllers/HomeController.cs
@@ -23,6 +23,12 @@ namespace MyPortfolio.Controllers
             return View();
         }
 
+        public IActionResult MyPortfolio()
+        {
+            ViewData["Title"] = "My Portfolio";
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
